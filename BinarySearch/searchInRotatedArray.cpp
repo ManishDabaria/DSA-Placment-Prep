37e6fd242556b56361733df1981e6b6a58521e5d
Manish Dabaria <manish786vermario@gmail.com>
2026-08-18 23:53:20 +0530

    Solve more questions in Binary Search

---
+++ BinarySearch/searchInRotatedArray.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int search(vector<int>&nums, int target){
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==target)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+int main(){
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    Solution sol;
+
+    int result = sol.search(nums, target);
+    
+    if(result == -1)
+        cout << "Target is not present.\n";
+    else
+        cout << "The index is: " << result << "\n";
+    return 0;
+
+}
